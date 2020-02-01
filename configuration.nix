c2f3e44ec053a15039ea3e6ae162cb401fc2e554
Thomas Pham <tpham@ithings.ch>
2020-02-01 11:59:52 +0100

    Create a custom Openstack Image

--- configuration.nix
+++ configuration.nix
@@ -1,11 +1,45 @@
 { config, lib, pkgs, ... }:
-{
-  services.sshd.enable = true;
-  services.nginx.enable = true;
+let
 
-  networking.firewall.allowedTCPPorts = [ 80 ];
-  
+  profiles = (
+    if (builtins.pathExists ./deployment.nix) then
+      ./custom-additions.nix
+      ./deployment.nix
+    else
+      ./custom-additions.nix
+  );
+
+in {
+  imports = [
+    <nixpkgs/nixos/modules/virtualisation/openstack-config.nix>
+    profiles
+  ];
+
+  environment.etc = {
+    custom-additions = {
+      source = ./custom-additions.nix;
+      target = "nixos/custom-additions.nix";
+      mode = "0600";
+    };
+  };
+
+  system.activationScripts.getty = ''
+    mkdir -p /sbin
+    ln -sfn ${pkgs.utillinux}/sbin/agetty /sbin/agetty
+  '';
+  systemd.services."autovt@".enable = lib.mkForce true;
+
+  services.cloud-init.ext4.enable = true;
+
+  networking.timeServers = [ "10.1.1.101" "10.1.1.102" ];
+  services.logrotate.enable = true;
+
+  users.motd = "Restricted Access Only";
+  ## Add your changes below
+
+  networking.firewall.allowedTCPPorts = [ 80 ];  
   users.users.root.password = "nixos";
   services.openssh.permitRootLogin = lib.mkDefault "yes";
-  services.getty.autologinUser = lib.mkDefault "root";
+  services.mingetty.autologinUser = lib.mkDefault "root";
+
 }
--- formats/openstack.nix
+++ formats/openstack.nix
@@ -1,4 +1,4 @@
-{ modulesPath, lib, ... }:
+{ modulesPath, config, lib, pkgs, ... }:
 if lib.pathExists "${toString modulesPath}/../maintainers/scripts/openstack/nova-image.nix" then {
   imports = [
     "${toString modulesPath}/../maintainers/scripts/openstack/nova-image.nix"
@@ -9,5 +9,35 @@ if lib.pathExists "${toString modulesPath}/../maintainers/scripts/openstack/nova
   imports = [
     "${toString modulesPath}/../maintainers/scripts/openstack/openstack-image.nix"
   ];
+
+  system.build.openstackImage = import "${toString modulesPath}/../lib/make-disk-image.nix" {
+    inherit lib config;
+    pkgs = import "${toString modulesPath}/../.." { inherit (pkgs) system; }; # ensure we use the regular qemu-kvm package
+    diskSize = 8192;
+    format = "qcow2";
+    configFile = pkgs.writeText "configuration.nix"
+      ''
+        { config, lib, pkgs, ... }:
+        let
+
+          profiles = (
+            if (builtins.pathExists ./deployment.nix) then
+              ./custom-additions.nix
+              ./deployment.nix
+            else
+              ./custom-additions.nix
+          );
+
+        in {
+          imports = [
+            <nixpkgs/nixos/modules/virtualisation/openstack-config.nix>
+            profiles
+          ];
+          ## Add your changes below
+
+        }
+      '';
+  };
+
   formatAttr = "openstackImage";
 }
