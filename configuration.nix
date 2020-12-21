1e9385ba79013c4bd9c65949ce640cb6864b16cd
Thomas Pham <tpham@ithings.ch>
2020-12-21 13:48:21 +0100

    refactor for custom-additions and deployment.nix import

--- configuration.nix
+++ configuration.nix
@@ -1,32 +1,26 @@
 { config, lib, pkgs, ... }:
 let
 
-in {
-  imports = [ <nixpkgs/nixos/modules/virtualisation/openstack-config.nix> ];
+  profiles = (
+    if (builtins.pathExists ./deployment.nix) then
+      ./custom-additions.nix
+      ./deployment.nix
+    else
+      ./custom-additions.nix
+  );
 
-  boot = {
-    kernelParams = lib.mkForce [ "console=tty1" "console=ttyS0" "panic=1" "boot.panic_on_fail" "LANG=en_US.UTF-8" ];
-    initrd.kernelModules = [ "virtio_scsi" ];
-    kernelModules = [ "virtio_pci" "virtio_net" ];
-    loader = {
-      timeout = lib.mkForce 5;
-      # because our image use virtio-scsi driver.
-      grub.device = lib.mkForce "/dev/sda"; # default vda
+in {
+  imports = [
+    <nixpkgs/nixos/modules/virtualisation/openstack-config.nix>
+    profiles
+  ];
+
+  environment.etc = {
+    custom-additions = {
+      source = ./custom-additions.nix;
+      target = "nixos/custom-additions.nix";
+      mode = "0600";
     };
   };
-
-  system.activationScripts.getty = ''
-    mkdir -p /sbin
-    ln -sfn ${pkgs.utillinux}/sbin/agetty /sbin/agetty
-  '';
-  systemd.services."autovt@".enable = lib.mkForce true;
-
-  services.cloud-init.ext4.enable = true;
-
-  networking.timeServers = [ "10.1.1.101" "10.1.1.102" ];
-  services.logrotate.enable = true;
-
-  users.motd = "Restricted Access Only";
-  ## Add your changes below
 
 }
---
+++ custom-additions.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:
+let
+
+in {
+  boot = {
+    kernelParams = lib.mkForce [ "console=tty1" "console=ttyS0" "panic=1" "boot.panic_on_fail" "LANG=en_US.UTF-8" ];
+    initrd.kernelModules = [ "virtio_scsi" ];
+    kernelModules = [ "virtio_pci" "virtio_net" ];
+    loader = {
+      timeout = lib.mkForce 5;
+      # because our image use virtio-scsi driver.
+      grub.device = lib.mkForce "/dev/sda"; # default vda
+    };
+  };
+
+  system.activationScripts.getty = ''
+    mkdir -p /sbin
+    ln -sfn ${pkgs.utillinux}/sbin/agetty /sbin/agetty
+  '';
+  systemd.services."autovt@".enable = lib.mkForce true;
+
+  services.cloud-init.ext4.enable = true;
+
+  networking.timeServers = [ "10.1.1.101" "10.1.1.102" ];
+  services.logrotate.enable = true;
+
+  documentation.nixos.enable = false;
+  
+  environment.systemPackages = with pkgs; [
+    curl jq wget tmux nload nmon 
+  ];
+
+  users.motd = "Restricted Access Only";
+}
